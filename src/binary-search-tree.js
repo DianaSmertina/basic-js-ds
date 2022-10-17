8d1448bc7495dbf36df7491713ad9a581751cd7b
DianaSmertina <am.dianasmertina@gmail.com>
2022-10-18 01:46:47 +0400

    feat: add dessision for binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,125 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootNode = addMain(data, this.rootNode);
+
+    function addMain (data, node) {
+      if (node === null) {
+        return new Node(data);
+      } else if (data === node.data) {
+        return node;
+      } else if (data > node.data) {
+        node.right = addMain(data, node.right);
+      } else {
+        node.left = addMain(data, node.left);
+      }
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    function hasMain(data, node) {
+      if (node === null) {
+        return false;
+      } else if (data === node.data) {
+        return true;
+      } else if (data > node.data) {
+        return hasMain(data, node.right);
+      } else {
+        return hasMain(data, node.left);
+      }
+    }
+    return hasMain(data, this.rootNode);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    function findMain(data, node) {
+      if (node === null) {
+        return null;
+      } else if (data === node.data) {
+        return node;
+      } else if (data > node.data) {
+        return findMain(data, node.right);
+      } else {
+        return findMain(data, node.left);
+      }
+    }
+    return findMain(data, this.rootNode);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    
+    function removeMain (data, node) {
+      if (node === null) {
+        return 0;
+      } else if (data > node.data) {
+          node.right = removeMain(data, node.right);
+          return node;
+        } else if (data < node.data) {
+          node.left = removeMain(data, node.left);
+          return node;
+        } else {
+          if (node.left === null && node.right === null) {
+              return null;
+            } else if (node.left === null) {
+              node = node.right;
+              return node;
+            } else if (node.right === null) {
+              node = node.left;
+              return node;
+            } else {
+              let rightChild = node.right;
+              console.log(node, rightChild)
+              while (rightChild.left !== null) {
+                      rightChild = rightChild.left;
+                  }
+              node.data = rightChild.data;
+              node.right = removeMain(rightChild.data, node.right);
+              return node;
+            }
+        }
+    }
+    this.rootNode = removeMain(data, this.rootNode);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return mainMin(this.rootNode);
+    function mainMin (node) {
+      if (node.left !== null) {
+        return mainMin(node.left);
+      } else {
+        return node.data;
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return mainMax(this.rootNode);
+    function mainMax (node) {
+      if (node.right !== null) {
+        return mainMax(node.right);
+      } else {
+        return node.data;
+      }
+    }
   }
 }
 
